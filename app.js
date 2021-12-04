915446f5fbabfa07d0a09d3239a1a53cf4d4bad0
E-Martinez77 <emartizzle2185@me.com>
2021-12-04 10:30:42 -0700

    multiplication function

--- app.js
+++ app.js
@@ -1,6 +1,63 @@
 const start = document.getElementById("start");
 
-function multiply(howMany) {}
+function multiply(howMany) {
+  const total = [];
+  const correct = [];
+  const incorrect = [];
+  const review = [];
+
+  for (let i = 0; i < howMany; i++) {
+    let num1 = Math.floor(Math.random() * 12) + 1;
+    let num2 = Math.floor(Math.random() * 12) + 1;
+
+    if (
+      num1 == 1 ||
+      num1 == 10 ||
+      num1 == 11 ||
+      num2 == 1 ||
+      num2 == 10 ||
+      num2 == 11
+    ) {
+      i--;
+    } else {
+      let question = `${num1} x ${num2} = ?`;
+      let userQuestion = prompt(question);
+      let answer = num1 * num2;
+
+      let newObj = {
+        q: question,
+        k: answer,
+        a: userQuestion,
+      };
+
+      total.push(newObj);
+    }
+  }
+
+  total.forEach((index) => {
+    if (index.a == index.k) {
+      correct.push(index);
+    } else {
+      incorrect.push(index);
+    }
+  });
+
+  incorrect.forEach((index) => {
+    review.push(index.q);
+  });
+
+  console.log(
+    `You got ${correct.length} out of ${
+      total.length
+    } correct,\nYou scored ${Math.round(
+      (correct.length / total.length) * 100
+    )}%\n ${
+      correct.length == total.length
+        ? "Wow! You got them all!"
+        : "Here are the problems you missed\n" + review
+    }`
+  );
+}
 
 function newGame(getStarted, howMany) {
   switch (getStarted) {
